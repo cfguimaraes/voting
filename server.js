3f623a34d92709ccf5c4c584188802cbdb0f3ef2
Cleverton Fernandes Guimarães <clevertonfernandesguimaraes@gmail.com>
2018-03-13 15:16:50 -0300

    Trying to identity where error is located

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-/* server.js */
+// /* server.js */
 
-// import the native http module
-var http = require('http')
+// // import the native http module
+// var http = require('http')
 
-// import gun
-var Gun = require('gun')
+// // import gun
+// var Gun = require('gun')
 
-var server = http.createServer(function(req, res){
-  if (Gun.serve(req, res)){ return } // filters gun requests!
-  require('fs').createReadStream(require('path').join(__dirname, req.url)).on('error',function(){ // static files!
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(
-    require('fs')
-      .readFileSync(require('path')
-      .join(__dirname, 'index.html') // or default to index
-    ));
-  }).pipe(res); // stream
-});
+// var server = http.createServer(function(req, res){
+//   if (Gun.serve(req, res)){ return } // filters gun requests!
+//   require('fs').createReadStream(require('path').join(__dirname, req.url)).on('error',function(){ // static files!
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end(
+//     require('fs')
+//       .readFileSync(require('path')
+//       .join(__dirname, 'index.html') // or default to index
+//     ));
+//   }).pipe(res); // stream
+// });
 
-server.listen(80)
+// server.listen(80)
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(80); 
