f219bd1b8591313f4752e602eb7e0a5d126ef85e
Cleverton Fernandes Guimarães <clevertonfernandesguimaraes@gmail.com>
2018-03-13 12:08:14 -0300

    Added node server

---
+++ server.js
@@ -0,0 +1,20 @@
+var port = 80;
+
+var Gun = require('../');
+
+var server = require('http').createServer(function(req, res){
+	if(Gun.serve(req, res)){ return } // filters gun requests!
+	require('fs').createReadStream(require('path').join(__dirname, req.url)).on('error',function(){ // static files!
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(require('fs')
+			.readFileSync(require('path')
+			.join(__dirname, 'index.html') // or default to index
+		));
+	}).pipe(res); // stream
+});
+
+var gun = Gun({ 
+	web: server	
+});
+
+server.listen(port);
