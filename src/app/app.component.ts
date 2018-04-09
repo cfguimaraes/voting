1478fcf60367c91cf31b985b7f79e05b74247b6b
Cleverton Fernandes Guimarães <clevertonfernandesguimaraes@gmail.com>
2018-04-09 19:11:15 -0300

    Corrected versions & package dependecies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component } from "@angular/core";
-import { SplashScreen } from "@ionic-native/splash-screen";
-import { StatusBar } from "@ionic-native/status-bar";
-import { Platform } from "ionic-angular";
+import { Component } from '@angular/core';
 
-import { TabsPage } from "./../pages/tabs/tabs";
+import { TabsPage } from './../pages/tabs/tabs';
 
 @Component({
     templateUrl: "app.html"
 })
 export class MyApp {
     rootPage: any = TabsPage;
-
-    constructor(
-        platform: Platform,
-        statusBar: StatusBar,
-        splashScreen: SplashScreen
-    ) {
-        platform.ready().then(() => {
-            if (platform.is("cordova")) {
-                statusBar.styleDefault();
-                splashScreen.hide();
-            }
-        });
-    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
@@ -16,8 +14,6 @@ import { MyApp } from './app.component';
     bootstrap: [IonicApp],
     entryComponents: [MyApp, TabsPage, HomePage, UpvotesPage],
     providers: [
-        StatusBar,
-        SplashScreen,
         { provide: ErrorHandler, useClass: IonicErrorHandler },
         GunProvider
     ]
